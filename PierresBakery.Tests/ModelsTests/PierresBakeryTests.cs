d36bd729b505d8e01d418f8cefed7e19d4870bee
TrevorH08 <trevorh8700@gmail.com>
2022-09-23 17:08:25 -0700

    WIP trying to get tests to work

---
+++ PierresBakery.Tests/ModelsTests/PierresBakeryTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisalStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+namespace PierresBakery.Models.Tests
+{
+  [TestClass]
+  public class BreadCostTests
+  {
+    [TestMethod]
+    public void Bread_BreadQuantityDivisibleByThree_True()
+    {
+      // Bread testBread = new Bread();
+      // Assert.AreEqual(true, testBread.BreadCost(3));
+    }
+  }
+}
