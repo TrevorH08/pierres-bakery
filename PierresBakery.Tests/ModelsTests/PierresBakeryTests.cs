f91760927624684409694a3a2a9ea9b56be5b8ea
TrevorH08 <trevorh8700@gmail.com>
2022-10-03 04:33:11 -0700

    add passing test for Bread price without discount

--- PierresBakery.Tests/ModelsTests/PierresBakeryTests.cs
+++ PierresBakery.Tests/ModelsTests/PierresBakeryTests.cs
@@ -13,6 +13,12 @@ namespace PierresBakery.Models.Tests
       Assert.AreEqual(3, testBread.BreadQuantity);
     }
     [TestMethod]
+    public void Bread_BreadQuantityMultipliableByPriceWithoutDiscount_True()
+    {
+      Bread testBread = new Bread(2);
+      Assert.AreEqual(10, testBread.BreadCost());
+    }
+    [TestMethod]
     public void Bread_BreadQuantityDivisibleByThree_True()
     {
       Bread testBread = new Bread(3);
